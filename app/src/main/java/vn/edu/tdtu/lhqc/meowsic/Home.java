beea2b58d8f6f2ed2834adb9fdc77fd3181567a8
perk271 <hohalokaka@gmail.com>
2025-09-29 18:30:40 +0700

    CHANGED: [Home][Removed Redundant Code]

--- app/src/main/java/vn/edu/tdtu/lhqc/meowsic/Home.java
+++ app/src/main/java/vn/edu/tdtu/lhqc/meowsic/Home.java
@@ -13,7 +13,7 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_home);
     }
 }
