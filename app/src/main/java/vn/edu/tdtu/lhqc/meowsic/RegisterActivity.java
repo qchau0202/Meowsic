ea9035a044217919b8acc0699a782d5c1ea4dae0
iamchouty <90039986+qchau0202@users.noreply.github.com>
2025-10-01 18:38:52 +0700

    ADDED: [Splash view][Define global colors, app icon]

--- app/src/main/java/vn/edu/tdtu/lhqc/meowsic/RegisterActivity.java
+++ app/src/main/java/vn/edu/tdtu/lhqc/meowsic/RegisterActivity.java
@@ -4,17 +4,14 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class SignUp extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_register);
 
     }
 }
