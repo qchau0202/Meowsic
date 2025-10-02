1259102eb45609a74c7d1a8d832cfa753db0479c
ngthtnhung <ngthtnhung@gmail.com>
2025-10-02 23:08:40 +0700

    ADDED: [SearchBar][Add searchbar and recycler view]

--- app/src/main/java/vn/edu/tdtu/lhqc/meowsic/fragments/fragment_library.java
+++ app/src/main/java/vn/edu/tdtu/lhqc/meowsic/fragments/fragment_library.java
@@ -1,66 +1,84 @@
 package vn.edu.tdtu.lhqc.meowsic.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.widget.SearchView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import vn.edu.tdtu.lhqc.meowsic.R;
+import vn.edu.tdtu.lhqc.meowsic.Song;
+import vn.edu.tdtu.lhqc.meowsic.SongAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragment_library#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class fragment_library extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public fragment_library() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_library.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static fragment_library newInstance(String param1, String param2) {
-        fragment_library fragment = new fragment_library();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_library, container, false);
+        View view = inflater.inflate(R.layout.fragment_library, container, false);
+
+        TextView textViewInfo = view.findViewById(R.id.textViewInfo);
+        ImageView btnSearch = view.findViewById(R.id.btnSearch);
+        SearchView searchView = view.findViewById(R.id.searchView);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerViewSongs);
+
+        // Fake danh sách bài hát
+        List<Song> songs = new ArrayList<>();
+        songs.add(new Song("Nắng Ấm Xa Dần", "Sơn Tùng MTP"));
+        songs.add(new Song("Lạc Trôi", "Sơn Tùng MTP"));
+        songs.add(new Song("Chúng Ta Không Thuộc Về Nhau", "Sơn Tùng MTP"));
+        songs.add(new Song("Em Của Ngày Hôm Qua", "Sơn Tùng MTP"));
+        songs.add(new Song("Chạy Ngay Đi", "Sơn Tùng MTP"));
+
+        // Setup RecyclerView
+        SongAdapter adapter = new SongAdapter(songs);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+
+        // Bấm icon kính lúp → hiện SearchView full, ẩn tiêu đề
+        btnSearch.setOnClickListener(v -> {
+            btnSearch.setVisibility(View.GONE);
+            textViewInfo.setVisibility(View.GONE);
+
+            searchView.setVisibility(View.VISIBLE);
+            searchView.setIconified(false);
+            searchView.requestFocus();
+        });
+
+        // Đóng search → hiện lại tiêu đề và icon
+        searchView.setOnCloseListener(() -> {
+            searchView.setVisibility(View.GONE);
+            btnSearch.setVisibility(View.VISIBLE);
+            textViewInfo.setVisibility(View.VISIBLE);
+            return false; // false để clear text
+        });
+
+        // Bắt sự kiện search
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                adapter.filter(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.filter(newText);
+                return true;
+            }
+        });
+
+        return view;
     }
 }
