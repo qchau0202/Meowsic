d54589d904eabdccc0ef45702513a813baf354d9
perk271 <hohalokaka@gmail.com>
2025-10-14 10:46:40 +0700

    CHANGED: [AUTHENTICATION][ADDED Registration Function, Fixed inputType for Name]

--- app/src/main/java/vn/edu/tdtu/lhqc/meowsic/activities/RegisterActivity.java
+++ app/src/main/java/vn/edu/tdtu/lhqc/meowsic/activities/RegisterActivity.java
@@ -1,23 +1,73 @@
 package vn.edu.tdtu.lhqc.meowsic.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import vn.edu.tdtu.lhqc.meowsic.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private TextInputEditText edtRegisterName, edtRegisterEmail, edtRegisterPassword;
+    private Button btnRegister;
+    private TextView tvNavigateToLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
 
-        findViewById(R.id.register_btn).setOnClickListener(v -> {
-            startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+        edtRegisterName = findViewById(R.id.edtRegisterName);
+        edtRegisterEmail = findViewById(R.id.edtRegisterEmail);
+        edtRegisterPassword = findViewById(R.id.edtRegisterPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+        tvNavigateToLogin = findViewById(R.id.tvNavigateToLogin);
+
+        btnRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String name = edtRegisterName.getText().toString();
+                String email = edtRegisterEmail.getText().toString();
+                String password = edtRegisterPassword.getText().toString();
+
+                if(name.isEmpty() || email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "Please fill in all fields,", Toast.LENGTH_SHORT).show();
+                } else {
+                    SharedPreferences sharedPreferences = getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                    editor.putString("name", name);
+                    editor.putString("email", email);
+                    editor.putString("password", password);
+
+                    editor.apply();
+
+                    Toast.makeText(RegisterActivity.this, "Register Successfully", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(intent);
+
+                    finish();
+                }
+            }
+        });
+        tvNavigateToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
